3dec734c1c83cd7872b7cddbccd5ab17e897dd89
taylor-a-barnes <taylor.a.barnes@gmail.com>
2020-11-04 15:32:01 -0500

    Add support for build_image script

--- mdimechanic/install.py
+++ mdimechanic/install.py
@@ -7,16 +7,30 @@ def install_all( base_path ):
     yaml_path = os.path.join( base_path, "mdimechanic.yml" )
     with open(yaml_path, "r") as yaml_file:
         mdimechanic_yaml = yaml.load(yaml_file, Loader=yaml.FullLoader)
-    build_lines = mdimechanic_yaml['docker']['build_engine']
-    build_script = ''
-    for line in build_lines:
-        build_script += line + '\n'
 
-    script_path = os.path.join( base_path, ".mdimechanic", ".temp", "build_engine.sh" )
-    os.makedirs(os.path.dirname(script_path), exist_ok=True)
-    with open(script_path, "w") as script_file:
-        script_file.write( build_script )
-    print("YAML: " + str(build_script) )
+    # Read the script to build the image from the yaml file
+    build_image_lines = mdimechanic_yaml['docker']['build_image']
+    build_image_script = ''
+    for line in build_image_lines:
+        build_image_script += line + '\n'
+
+    # Write the script to build the image
+    image_script_path = os.path.join( base_path, "docker", ".temp", "build_image.sh" )
+    os.makedirs(os.path.dirname(image_script_path), exist_ok=True)
+    with open(image_script_path, "w") as script_file:
+        script_file.write( build_image_script )
+
+    # Read the script to build the engine from the yaml file
+    build_engine_lines = mdimechanic_yaml['docker']['build_engine']
+    build_engine_script = ''
+    for line in build_engine_lines:
+        build_engine_script += line + '\n'
+
+    # Write the script to build the engine
+    engine_script_path = os.path.join( base_path, ".mdimechanic", ".temp", "build_engine.sh" )
+    os.makedirs(os.path.dirname(engine_script_path), exist_ok=True)
+    with open(engine_script_path, "w") as script_file:
+        script_file.write( build_engine_script )
 
     # Switch to the package directory
     package_path = ut.get_package_path()
@@ -36,7 +50,7 @@ def install_all( base_path ):
     os.chdir(base_path)
 
     # Build the engine image
-    ret = os.system("docker build -t mdi_mechanic/lammps user/docker")
+    ret = os.system("docker build -t mdi_mechanic/lammps docker")
     if ret != 0:
         raise Exception("Unable to build the engine image")
 
