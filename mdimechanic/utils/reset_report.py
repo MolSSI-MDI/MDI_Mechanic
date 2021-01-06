f10fdd901e1dd13bd95178980387e095c628c7c4
taylor-a-barnes <taylor.a.barnes@gmail.com>
2021-01-06 12:39:26 -0500

    Add prepend section to README.md

--- mdimechanic/utils/reset_report.py
+++ mdimechanic/utils/reset_report.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-from .utils import get_package_path, get_mdimechanic_yaml
+from .utils import get_package_path, get_mdimechanic_yaml, insert_list
 
 def reset_report( base_path ):
     # Path to this file
@@ -24,14 +24,50 @@ def reset_report( base_path ):
     dst_location = os.path.join( base_path, "report")
     shutil.copytree( src_location, dst_location )
 
-    # Reset the README.md file
+    # Get the README.md file locations
     src_location = os.path.join( package_path, "mdimechanic", "report", "README.base" )
     dst_location = os.path.join( base_path, "README.md")
-    with open( src_location, 'r' ) as base_file: base = base_file.read()
+
+    # Get header information for the README
     code_name = mdimechanic_yaml['code_name']
-    prepend_data = "# MDI Mechanic " + str(code_name) + " report\n\n" + "This repo presents test results for the MDI interface implementation in the " + str(code_name) + " code.\n\n"
-    with open( dst_location, 'w' ) as new_file: new_file.write( prepend_data + base )
-    #shutil.copyfile( src_location, dst_location )
+    readme_data = ["# MDI Mechanic " + str(code_name) + " report\n",
+                    "\n",
+                    "This repo presents test results for the MDI interface implementation in the " + str(code_name) + " code.\n",
+                    "\n"]
+
+    # Get the body information for the README
+    with open( src_location, 'r' ) as base_file:
+        body = base_file.readlines()
+
+    # Add any prepended information from the mdimechanic.yml file
+    prepend_iline = 0
+    prepend_data = []
+    for iline in range(len(body)):
+        line = body[iline]
+        sline = line.split()
+        print("SLINE: " + str(sline))
+        if len(sline) > 3:
+            if sline[0] == '[yaml]:':
+                print("YAML")
+                instruction = sline[3]
+                if instruction == "prepend":
+                    prepend_iline = iline
+                    print("prepending")
+    if 'report' in mdimechanic_yaml:
+        if 'prepend' in mdimechanic_yaml['report']:
+            prepend_data = mdimechanic_yaml['report']['prepend']
+            for iline in range(len(prepend_data)):
+                if prepend_data[iline] == None:
+                    prepend_data[iline] = ""
+                prepend_data[iline] = prepend_data[iline] + "\n"
+            insert_list( body, prepend_data, prepend_iline)
+
+    # Combine the header data with the body of the README
+    readme_data.extend( body )
+
+    # Write the README.md file
+    with open( dst_location, 'w' ) as new_file:
+        new_file.writelines( readme_data )
 
 if __name__ == "__main__":
     reset_report()
