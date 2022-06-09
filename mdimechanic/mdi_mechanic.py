9de28637694e0eecbc3a61867516bd342e5c4cbb
Taylor Barnes <taylor.a.barnes@gmail.com>
2022-06-09 14:14:19 -0400

    Remove obsolete functions

--- mdimechanic/mdi_mechanic.py
+++ mdimechanic/mdi_mechanic.py
@@ -17,31 +17,15 @@ from . import cmd_interactive
 from .utils import tests as mtests
 from .utils import utils as ut
 
-def get_calling_path():
-    # Get the name of the file that called the calling function
-    caller_name = traceback.extract_stack()[-3][0]
-
-    # Get the path to the file that called this function
-    caller_path = os.path.realpath( caller_name )
-    caller_directory = os.path.dirname( caller_path )
-    
-    return caller_directory
-    
-
-
 def command_report():
     print("Starting a report")
     report_dir = os.getcwd()
     cmd_report.generate_report( report_dir )
 
-
-
 def command_build():
     print("Starting the installation")
     report_dir = os.getcwd()
     cmd_build.install_all( report_dir )
-
-
 
 def command_startproject( args ):
     print("Starting a new MDI project")
@@ -73,30 +57,6 @@ def command_startproject( args ):
 
     else:
         raise Exception("Unrecognized project type.")
-
-
-
-def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format)
-
-    Replace this function and doc string for your own project
-
-    Parameters
-    ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from
-
-    Returns
-    -------
-    quote : str
-        Compiled string including quote and optional attribution
-    """
-
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
 
 def command_run( args ):
     run_dir = os.getcwd()
@@ -133,8 +93,3 @@ def command_interactive( ):
         print("Success: Interactive session completed successfully.")
     except:
         raise Exception("Error: Interactive session did not complete successfully.")
-
-
-if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())
